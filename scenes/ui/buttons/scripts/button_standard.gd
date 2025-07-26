feaac0dc20863138ee51204ea838db7ebe5f2758
MrEliptik <victormeunier.dev@gmail.com>
2025-07-26 18:18:09 +0200

    default scenes

---
+++ scenes/ui/buttons/scripts/button_standard.gd
@@ -0,0 +1,10 @@
+class_name ButtonStandard extends Button
+
+func _ready() -> void:
+	pass
+	
+func hover() -> void:
+	pass
+	
+func unhover() -> void:
+	pass
---
+++ scenes/ui/titlescreen/scripts/titlescreen.gd
@@ -0,0 +1,16 @@
+class_name Titlescreen extends Control
+
+@onready var buttons: VBoxContainer = $Buttons
+
+func _on_btn_start_pressed() -> void:
+	SceneSwitcher.switch_to(SceneSwitcher.SCENE.GAME, true)
+
+func _on_btn_settings_pressed() -> void:
+	SceneSwitcher.switch_to(SceneSwitcher.SCENE.SETTINGS, true)
+
+func _on_btn_credits_pressed() -> void:
+	SceneSwitcher.switch_to(SceneSwitcher.SCENE.CREDITS, true)
+
+func _on_btn_quit_pressed() -> void:
+	# Can add a confirmation or 
+	Utils.exit_game()
