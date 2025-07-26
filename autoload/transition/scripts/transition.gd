c68a22f40e22316e281ace2512c202e5e4ececf6
MrEliptik <victormeunier.dev@gmail.com>
2025-07-26 17:15:13 +0200

    circle transition

--- autoload/transition/scripts/transition.gd
+++ autoload/transition/scripts/transition.gd
@@ -2,3 +2,25 @@ extends Node
 
 signal transition_in_finished() 
 signal transition_out_finished()
+
+@onready var circle: ColorRect = $CanvasLayer/Circle
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+var shown: bool = false
+
+func _ready() -> void:
+	pass # Replace with function body.
+
+func show_transition() -> void:
+	if shown: return
+	circle.material.set_shader_param("screen_width", circle.size.x)
+	circle.material.set_shader_param("screen_height", circle.size.y)
+	shown = true
+	animation_player.play("show")
+	
+func hide_transition() -> void:
+	if not shown: return
+	circle.material.set_shader_param("screen_width", circle.size.x)
+	circle.material.set_shader_param("screen_height", circle.size.y)
+	shown = false
+	animation_player.play("hide")
