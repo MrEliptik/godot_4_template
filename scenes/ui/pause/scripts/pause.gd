2684683fb47cdb82c78649b0bc90bc8dec28b4a8
MrEliptik <victormeunier.dev@gmail.com>
2025-07-28 11:54:13 +0200

    pause

--- scenes/ui/pause/scripts/pause.gd
+++ scenes/ui/pause/scripts/pause.gd
@@ -1,5 +1,25 @@
 class_name PauseUI extends Control
 
+@onready var scene: Control = $Scene
+
+func _ready() -> void:
+	hide()
+
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("pause"):
-		get_tree().paused = not get_tree().paused
+		pause(not get_tree().paused)
+
+func pause(pause_state: bool) -> void:
+	get_tree().paused = pause_state
+	visible = pause_state
+
+func _on_btn_continue_pressed() -> void:
+	pause(false)
+
+func _on_btn_settings_pressed() -> void:
+	var instance: SettingsUI = SceneSwitcher.instance_scene(SceneSwitcher.SCENE.SETTINGS, {"standalone": true})
+	scene.add_child(instance)
+
+func _on_btn_home_pressed() -> void:
+	pause(false)
+	SceneSwitcher.switch_to(SceneSwitcher.SCENE.TITLE)
--- scenes/ui/settings/scripts/settings_ui.gd
+++ scenes/ui/settings/scripts/settings_ui.gd
@@ -1,4 +1,19 @@
 class_name SettingsUI extends Control
 
+signal done()
+
+@export var standalone: bool = true
+
+func _ready() -> void:
+	if has_meta("data") and get_meta("data").has("standalone"):
+		standalone = get_meta("data")["standalone"]
+
+func disappear() -> void:
+	queue_free()
+
 func _on_btn_home_pressed() -> void:
-	SceneSwitcher.switch_to(SceneSwitcher.SCENE.TITLE, false)
+	if standalone:
+		SceneSwitcher.switch_to(SceneSwitcher.SCENE.TITLE, false)
+	else:
+		done.emit()
+		disappear()
